2cd6ba94c850ee10dc84650858c9f82705cec3db
hearsute <manuoval@gmail.com>
2014-10-01 11:00:59 +0200

    Initial commit

---
+++ module/Mur/config/module.config.php
@@ -0,0 +1,91 @@
+<?php
+
+
+return [
+    'controllers' =>
+        [
+            'invokables' =>
+                [
+                    'Mur\Controller\User' => 'Mur\Controller\UserController'
+                ],
+        ],
+    'router' =>
+        [
+            'routes' =>
+                [
+                    'home' =>
+                        [
+                            'type' => 'Zend\Mvc\Router\Http\Literal',
+                            'options' =>
+                                [
+                                    'route' => '/',
+                                    'defaults' =>
+                                        [
+                                            'controller' => 'Mur\Controller\User',
+                                            'action' => 'index',
+                                        ],
+                                ],
+                        ],
+
+                    'mur' =>
+                        [
+                            'type' => 'Zend\Mvc\Router\Http\Literal',
+                            'options' =>
+                                [
+                                    'route' => '/mur',
+                                    'defaults' =>
+                                        [
+                                            '__NAMESPACE__' => 'Mur\Controller',
+                                            'controller' => 'User',
+                                            'action' => 'index',
+                                        ],
+
+                                ],
+                        ],
+
+                ],
+        ],
+    'service_manager' => [
+        'abstract_factories' => [
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory',
+        ],
+        'aliases' => [
+            'translator' => 'MvcTranslator',
+        ],
+    ],
+    'translator' => [
+        'locale' => 'en_US',
+        'translation_file_patterns' => [
+            [
+                'type' => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern' => '%s.mo',
+            ],
+        ],
+    ],
+
+    'view_manager' => [
+        'display_not_found_reason' => true,
+        'display_exceptions' => true,
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
+        'template_map' => [
+            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+            'mur/user/index' => __DIR__ . '/../view/mur/user/index.phtml',
+            'error/404' => __DIR__ . '/../view/error/404.phtml',
+            'error/index' => __DIR__ . '/../view/error/index.phtml',
+        ],
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ],
+    // Placeholder for console routes
+    'console' => [
+        'router' => [
+            'routes' => [
+            ],
+        ],
+    ],
+];
---
+++ module/Mur/src/Mur/Controller/UserController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Manu
+ * Date: 01/10/2014
+ * Time: 09:59
+ */
+
+namespace Mur\Controller;
+
+use Zend\View\Model\ViewModel;
+use Zend\Mvc\Controller\AbstractActionController;
+
+class UserController extends AbstractActionController
+{
+    /**
+     * @return ViewModel
+     */
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+    public function createAction()
+    {
+
+    }
+
+    public function updateAction()
+    {
+
+    }
+
+    public function deleteAction()
+    {
+
+    }
+} 
